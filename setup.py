bd030cf3c20e6697c9f2a34d628506f03074407b
EGS <egs@theleys.net>
2021-01-28 09:43:14 +0000

    Add project URL

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
         name='aqa32',
-        version='4.4',
+        version='4.5',
         description='AQA32 Instruction Set: Assembler and Emulator',
+        url='https://github.com/speyermr/aqa32',
         author='E G Speyer',
         author_email='python@edwardspeyer.com',
         license='MIT',
